ac02a7987726778a42d0622a484539b5b5489490
andertoni1183 <101605808+andertoni1183@users.noreply.github.com>
2022-07-01 14:59:12 +1200

    Add files via upload

--- 00_factors_calc.py
+++ 00_factors_calc.py
@@ -33,3 +33,89 @@ def instructions():
 
 
 
+# checks input is anumber more than a given value
+def num_check(question, low):
+    valid = False
+    while not valid:
+
+        error = "Please enter an interger that is more than (or equal to) {}".format(low)
+
+        try:
+
+            # ask user to enter a number
+            response = int(input(question))
+
+            # checks number is more than zero
+            if response >= low:
+                return response
+
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+
+
+
+
+
+
+
+
+
+# Main Routine goes here
+
+# Heading
+statement_generator("Factors Calculator", "-")
+
+# Dispaly instructions if user has not used the program before
+first_time = input("Press <enter> to use instructions or any key to continue")
+
+if first_time == "":
+    instructions()
+
+# Loop to allow multiple calculations per session
+keep_going = ""
+while keep_going == "":
+
+    comment= ""
+
+    # ask user for number to be factored...
+    var_to_factor = num_check("Number? ") 
+    
+    if var_to_factor != 1:
+        factor_list = get_factors(var_to_factor)
+    else:
+        factor_list = ""
+        comment = "One is UNITY!  It only has one factor.  Itself :)"
+
+    # comments for squares / primes
+    if len(factor_list) == 2:
+        comment = "{} is a prime number.".format(var_to_factor)
+    elif len(factor_list) % 2 == 1:
+        comment = "{} is a perfect square.".format(var_to_factor)    
+
+    # output factors and comment
+
+    # Generate heading...
+    if var_to_factor == 1:
+        heading = "One is special..."
+
+    else:
+        heading = "Factors of {}".format(var_to_factor)    
+
+    #Output factors and comment
+    statement_generator(heading, "*")   
+    print()
+    print(factor_list) 
+    print(comment)
+
+    print()
+    keep_going = input("Press <enter> to continue or any key to quit")
+    print()
+
+
+print()
+print("Than you for using factors calculator")
